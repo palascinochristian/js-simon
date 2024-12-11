8ca40ec72a30d7fec6ec6a62be0a5c14f174c201
palascinochristian <palascinochristian@gmail.com>
2024-12-11 16:24:36 +0100

    feat: add result structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*-------------
+  DOM ELEMENTS
+ -------------- */
+
+ const randomNum = document.getElementById("randomNum");
+ const countdown = document.getElementById("timer");
+ const userForm = document.getElementById("userForm");
